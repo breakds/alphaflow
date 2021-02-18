1b413b9fffbd81f766f96d793bd54e7f99eaf8e9
Break Yang <breakds@gmail.com>
2021-02-18 08:29:28 -0800

    Add shell.nix.

---
+++ shell.nix
@@ -0,0 +1,17 @@
+let pkgs = import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/66acfa3d16eb599f5aa85bda153a24742f683383.tar.gz";
+    }) {
+      config.allowUnfree = true;
+    };
+
+    customizedPython = pkgs.python3.withPackages (python-packages: with python-packages;
+      let computation = [
+            numpy pandas
+          ];
+          viz = [ matplotlib tqdm ];
+      in computation ++ viz);
+
+in pkgs.mkShell rec {
+  name = "alphaflow";
+  buildInputs = with pkgs; [ customizedPython ];
+}
