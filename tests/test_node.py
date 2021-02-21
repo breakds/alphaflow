9a991e5a27d5d17bde95a3d2dc3121b5d86655ac
Break Yang <breakds@gmail.com>
2021-02-20 18:00:04 -0800

    Add recursive function in test

--- tests/test_node.py
+++ tests/test_node.py
@@ -41,6 +41,17 @@ class Sum(Node):
         return sum
 
 
+def create_recursive_sum(*args):
+    if len(args) == 0:
+        return Constant(0)
+    
+    func = args[0]
+    for arg in args[1:]:
+        func = Add(arg, func)
+
+    return func
+    
+
 class TestNode(unittest.TestCase):
     def test_implicit_node_name(self):
         a = Factorial(5)
@@ -61,7 +72,7 @@ class TestNode(unittest.TestCase):
         self.assertEqual(840, c.value())
 
 
-    def test_recursive_sum_four(self):
+    def test_sum_four(self):
         a = Factorial(1, name = 'F1!')
         b = Factorial(2, name = 'F2!')
         c = Factorial(3, name = 'F3!')
@@ -82,6 +93,15 @@ class TestNode(unittest.TestCase):
     def test_constant_node(self):
         a = Constant('Great!')
         self.assertEqual('Great!', a.value())
+
+
+    def test_recursive_sum(self):
+        a = Factorial(1, name = 'F1!')
+        b = Factorial(2, name = 'F2!')
+        c = Factorial(3, name = 'F3!')
+        d = Factorial(4, name = 'F4!')
+        result = Sum(a, b, c, d)
+        self.assertEqual(33, result.value())
 
 
 if __name__ == '__main__':
